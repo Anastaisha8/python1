35648119a3151f55ac037e4f4336f653b6272e40
Nasty <balandina2balandina@yandex.ru>
2022-12-08 10:46:54 +0300

    added new 2 examples

--- py2.py
+++ py2.py
@@ -1,27 +1,15 @@
-def inputK(x):
-    a = [0]*x 
-    for i in range(x):
-        is_True = False 
-        while not is_True:
-            try:
-                number = float(input(f"Введите {i+1} координату: "))
-                a[i] = number 
-                is_True = True
-                if a[i] == 0:
-                    is_True = False 
-                    print("Координата не может равняться 0")
-            except ValueError:
-                print("Ошибка!!! Необходимо ввести числа ")
-    return a
-
-def coord(xy):
-    text = 4 
-    if xy[0]<0 and xy[1]>0:
-        text = 2
-    elif xy[0]>0 and xy[1]>0:
-        text = 1
-    elif xy[0]< 0 and xy[1]<0:
-        text = 3 
-    print (f"Точка находится в {text} четверти плоскости")
-koord = inputK(2)
-coord(koord)
+def chetv(x,y):
+    if (x>0)and(y>0):
+        res= 1
+    if (x<0)and(y>0):
+        res= 2
+    if (x<0)and(y<0):
+        res = 2
+    if (x>0)and(y<0):
+        res = 4
+    
+    return res
+x = int(input("Введите первую координату:"))
+y = int(input("Введите вторую координату:"))
+res = chetv(x,y)
+print(f"Точка находится в {res} четверти плоскости")
